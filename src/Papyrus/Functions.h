3aa5145ee1e67cf7b7b8f4fd0f25a692c19b74d0
Scrabx3 <kris.vlntn@gmail.com>
2024-11-25 18:35:21 +0100

    move printconsole into general utility script

--- src/Papyrus/Functions.h
+++ src/Papyrus/Functions.h
@@ -1,26 +1,13 @@
 #pragma once
 
 #include "C3/Hooks/Hooks.h"
+#include "Util/Misc.h"
 
 namespace Papyrus::Functions
 {
 	void PrintConsole(RE::StaticFunctionTag*, std::string a_str)
   {
-		const auto console = RE::ConsoleLog::GetSingleton();
-		if (a_str.empty())
-			return;
-		else if (a_str.size() < 1000)
-			console->Print(a_str.data());
-		else {	// Large strings printed to console crash the game - truncate it
-			size_t i = 0;
-			do {
-				constexpr auto maxchar = 950;
-				auto print = a_str.substr(i, i + maxchar);
-				print += '\n';
-				i += maxchar;
-				console->Print(print.data());
-			} while (i < a_str.size());
-		}
+		Utility::PrintConsole(a_str);
 	}
 
 	void ExecuteCommand(STATICARGS, RE::BSFixedString command, RE::TESObjectREFR* target)
