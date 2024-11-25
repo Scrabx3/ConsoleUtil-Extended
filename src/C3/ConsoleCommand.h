9574646317ee73f8aeef9a0f3a0d4f68e1c66feb
Scrabx3 <kris.vlntn@gmail.com>
2024-11-25 11:38:41 +0100

    utility funcs for CoCos

--- src/C3/ConsoleCommand.h
+++ src/C3/ConsoleCommand.h
@@ -19,18 +19,26 @@ namespace C3
 		{
 			int integer;
 			float floating;
-			std::string string;
+			const char* string;
 		};
 
-		std::string name{};
+		RE::BSFixedString name{};
 		Type type{ Type::None };
 		Value value{ 0 };
+
+		bool ContainedBy(const Argument& a_rhs, bool exact) const;
+		bool operator==(const Argument& a_rhs) const;
 	};
 
 	struct ConsoleCommand
 	{
-		std::string name{};
+		RE::BSFixedString name{};
 		std::vector<Argument> arguments{};
+		RE::FormID target{ 0 };
+
+		bool ContainedBy(const ConsoleCommand& a_rhs, bool exact) const;
+		bool operator==(const ConsoleCommand& a_rhs) const { return name == a_rhs.name && target == a_rhs.target && arguments == a_rhs.arguments; }
+	};
 
 	/// @brief Create ConsoleCommand from a string
 	/// @param a_cmd The console input string
@@ -114,6 +122,70 @@ namespace C3
 			cmd.arguments.push_back(arg);
 		}
 		return cmd;
+	}
+
+	// Implementation of Argument
+
+	bool Argument::ContainedBy(const Argument& a_rhs, bool exact) const
+	{
+		if (exact) {
+			return *this == a_rhs;
+		} else if (!name.empty() && !a_rhs.name.contains(name)) {
+			return false;
+		} else if (type != a_rhs.type) {
+			return false;
+		}
+		switch (type) {
+		case Type::Integer:
+			return value.integer == a_rhs.value.integer;
+		case Type::Float:
+			return value.floating == a_rhs.value.floating;
+		case Type::String:
+			return _stricmp(value.string, a_rhs.value.string) == 0;
+		default:
+			return true;
+		}
+	}
+
+	bool Argument::operator==(const Argument& a_rhs) const
+	{
+		if (name != a_rhs.name || type != a_rhs.type) {
+			return false;
+		}
+		switch (type) {
+		case Type::Integer:
+			return value.integer == a_rhs.value.integer;
+		case Type::Float:
+			return value.floating == a_rhs.value.floating;
+		case Type::String:
+			return _stricmp(value.string, a_rhs.value.string) == 0;
+		default:
+			return true;
+		}
+	}
+
+	// Implementation of ConsoleCommand
+
+	bool ConsoleCommand::ContainedBy(const ConsoleCommand& a_rhs, bool exact) const
+	{
+		if (exact) {
+			if (name != a_rhs.name || arguments.size() != a_rhs.arguments.size()) {
+				return false;
+			}
+		} else {
+			if (!name.empty() && !a_rhs.name.contains(name)) {
+				return false;
+			}
+		}
+		if (target != 0 && target != a_rhs.target) {
+			return false;
+		}
+		for (const auto& arg : a_rhs.arguments) {
+			if (std::ranges::none_of(arguments, [&](const auto& a) { return a.ContainedBy(arg, exact); })) {
+				return false;
+			}
+		}
+		return true;
 	}
 
 }	 // namespace C3::Parser
